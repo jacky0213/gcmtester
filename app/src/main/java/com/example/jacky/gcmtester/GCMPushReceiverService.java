ea8884b06c687ab56dfd3c5595336eef0c37e5a9
Jacky <jacky_kckjy@yahoo.com.hk>
2016-07-18 18:25:15 +0800

    Update functionalities (Display Message)

--- app/src/main/java/com/example/jacky/gcmtester/GCMPushReceiverService.java
+++ app/src/main/java/com/example/jacky/gcmtester/GCMPushReceiverService.java
@@ -1,22 +1,34 @@
 package com.example.jacky.gcmtester;
 
+import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
 public class GCMPushReceiverService extends GcmListenerService {
+    public static final String MESSAGE_RECEIVED = "MessageReceived";
+
     @Override
     public void onMessageReceived(String from, Bundle data) {
         String message = data.getString("message");
         sendNotification(message);
+        Intent MsgRec = new Intent(MESSAGE_RECEIVED);
+        MsgRec.putExtra("message", message);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(MsgRec);
     }
+
     private void sendNotification(String message) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -35,5 +47,14 @@ public class GCMPushReceiverService extends GcmListenerService {
 
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, noBuilder.build()); //0 = ID of notification
+
+
+
+
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/jacky/gcmtester/MainActivity.java
+++ app/src/main/java/com/example/jacky/gcmtester/MainActivity.java
@@ -7,12 +7,15 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,18 +24,21 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import org.w3c.dom.Text;
+
 import java.io.IOException;
 
 public class  MainActivity extends Activity {
+    public static final String BROADCAST_RECEIVER = "BroadcastReceiver";
+    public int counter = 0;
+    public int maxNumOfMsg = 50;
     private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private BroadcastReceiver mMessageReceiver;
     private String token;
-    private GoogleCloudMessaging gcm;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        gcm = GoogleCloudMessaging.getInstance(this);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -46,14 +52,37 @@ public class  MainActivity extends Activity {
                     Toast.makeText(getApplicationContext(), "GCM token:" + token, Toast.LENGTH_LONG).show();
                     TextView tokenTv = (TextView) findViewById(R.id.tokenTv);
                     tokenTv.setText(token);
+                    Log.w(BROADCAST_RECEIVER, "REGISTRATION_SUCCESS");
+
                 } else if(intent.getAction().equals(GCMRegistrationIntentService.REGISTRATION_ERROR)){
                     //Registration error
                     Toast.makeText(getApplicationContext(), "GCM registration error!!!", Toast.LENGTH_LONG).show();
-                }else {
+                    Log.w(BROADCAST_RECEIVER, "REGISTRATION_ERROR");
 
+                }else {
                 }
             }
         };
+
+        final LinearLayout MessageLayout = (LinearLayout) findViewById(R.id.MessageLayout);
+
+        TextView tv = new TextView(this);
+        mMessageReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String message = intent.getStringExtra("message");
+                String[] msg = new String[maxNumOfMsg];
+
+                //Create TextView to display message on screen
+                if (counter < maxNumOfMsg) {
+                    msg[counter] = message;
+                    MessageLayout.addView(createTextView(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, "#" + counter + " " + msg[counter]));
+                    counter++;
+                }
+            }
+        };
+
+
 
         //Click "Copy" Button to copy the token shown
         Button copyBtn = (Button) findViewById(R.id.copyBtn);
@@ -67,6 +96,7 @@ public class  MainActivity extends Activity {
             }
         });
 
+        /*
         EditText sendMsgEt = (EditText) findViewById(R.id.sendMsgEt);
         Button sendMsgBtn = (Button) findViewById(R.id.sendMsgBtn);
         sendMsgBtn.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +105,7 @@ public class  MainActivity extends Activity {
 
             }
         });
+        */
 
         //Check status of Google play service in device
         int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
@@ -102,13 +133,41 @@ public class  MainActivity extends Activity {
                 new IntentFilter(GCMRegistrationIntentService.REGISTRATION_SUCCESS));
         LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
                 new IntentFilter(GCMRegistrationIntentService.REGISTRATION_ERROR));
-    }
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter(GCMPushReceiverService.MESSAGE_RECEIVED));
+
+}
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.w("MainActivity", "onPause");
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+    }
+
+    //Create TextView
+    protected TextView createTextView(int layout_widh, int layout_height, String text) {
+
+        TextView textView_item_name = new TextView(this);
+
+        // LayoutParams layoutParams = new LayoutParams(
+        // LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        // layoutParams.gravity = Gravity.LEFT;
+        LinearLayout.LayoutParams _params = new LinearLayout.LayoutParams(
+                layout_widh, layout_height);
+
+        //_params.setMargins(margin, margin, margin, margin);
+        //_params.addRule(align);
+        //textView_item_name.setLayoutParams(_params);
+
+        textView_item_name.setText(text);
+        //textView_item_name.setTextSize(TypedValue.COMPLEX_UNIT_SP, fontSize);
+        //textView_item_name.setTextColor(Color.parseColor("#000000"));
+        // textView1.setBackgroundColor(0xff66ff66); // hex color 0xAARRGGBB
+        //textView_item_name.setPadding(padding, padding, padding, padding);
+
+        return textView_item_name;
+
     }
 }
 
